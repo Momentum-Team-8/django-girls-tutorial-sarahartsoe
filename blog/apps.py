4be13c3c43be03be01f8e6ca803136310b1ee226
sarahartsoe <sarahartsoe@gmail.com>
2021-06-15 18:07:05 -0400

    corrected typos

--- blog/apps.py
+++ blog/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BloConfig(AppConfig):
+class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blo'
+    name = 'blog'
--- mysite/settings.py
+++ mysite/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'blog.urls'
+ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
     {
